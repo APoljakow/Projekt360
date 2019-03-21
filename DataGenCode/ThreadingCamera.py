3db68d9688accd175413b9d114a1cf2030742803
APoljakow <APoljakow@gmx.de>
2019-03-21 22:00:52 +0100

    compared available versions, added comments. FINAL

--- DataGenCode/ThreadingCamera.py
+++ DataGenCode/ThreadingCamera.py
@@ -2,6 +2,10 @@ import threading
 import RunCamera
 
 # Threading of running camera
+# final version of this class
+# to understand threads in python
+# https://docs.python.org/2.7/library/threading.html
+
 class cameraThread(threading.Thread):
     def __init__(self, threadName):
         threading.Thread.__init__(self)
